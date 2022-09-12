5a388d779bb400a0464e11acd1734bbc215a6f63
MrMarvel <seregakkk999@yandex.ru>
2022-09-12 03:42:05 +0300

    Добавил модуль очередей

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from module_controller import ModuleController
+from src.modules.queue_module.VkBot.src.queue_vk_bot_mrmarvel.app import QueueModule
 from src.modules.test_module.test_module import MyModule
 
 if __name__ == '__main__':
     module_controller = ModuleController()
-    module_controller.start_module(MyModule())
-    module_controller.start_module(MyModule())
-    module_controller.start_module(MyModule())
-    module_controller.start_module(MyModule())
-    module_controller.start_module(MyModule())
-    module_controller.destroy_modules()
+    module_controller.start_module(QueueModule())
